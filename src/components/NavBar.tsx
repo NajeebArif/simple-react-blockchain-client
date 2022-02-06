f0e61a67a377921196af1b6c9df7ae7f247ffbb6
Najeeb Arif <NajeebArif786@outlook.com>
2022-02-07 02:03:25 +0530

    Add the blockchain page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,39 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BlockchainNode, Transaction } from "../lib/blockchain-node";
+import Blockchain from "./Blockchain";
+import BlockchainClient from "./BlockchainClient";
+import Transactions from "./Transactions";
 
-type Props = {};
-
-export default function NavBar({}: Props) {
-  return (
-    <div id="nav-bar">
-      <nav className="bg-gradient-to-r from-slate-900  hover:bg-gradient-to-l hover:text-black text-white font-bold rounded-b shadow flex flex-row">
-        <NavLink href="#">Home</NavLink>
-        <NavLink href="#">Transactions</NavLink>
-        <NavLink href="#">Blockchain</NavLink>
-      </nav>
-    </div>
-  );
-}
-
-type NavProps = {
-  cssClasses?: string;
-  href?: string
+type Props = {
+  node: BlockchainNode,
+  generateBlock: ()=>void,
+  status: React.ReactElement,
+  addTransaction:  (transaction: Transaction) => void,
 };
 
-const NavLink: React.FC<NavProps> = (props) => {
+export default function NavBar({node, generateBlock, status, addTransaction}: Props) {
   const css = "hover:bg-gray-500 hover:rounded-b block p-3";
   return (
-    <>
-      <a
-        className={!!props.cssClasses ? props.cssClasses : css}
-        href={!!props.href ? props.href : "#"}
-      >
-        {props.children}
-      </a>
-    </>
+    <Router>
+      <div id="nav-bar">
+        <nav className="bg-gradient-to-r from-slate-900  hover:bg-gradient-to-l hover:text-black text-white font-bold rounded-b shadow flex flex-row">
+          <Link className={css} to="/">
+            Home
+          </Link>
+          <Link className={css} to="/transactions">
+            Transactions
+          </Link>
+          <Link className={css} to="/blockchain">
+            Blockchain
+          </Link>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/transactions" element={<Transactions />} />
+        <Route path="/blockchain" element={<Blockchain node={node}/>} />
+        <Route path="/" element={<BlockchainClient node={node} status={status} generateBlock={generateBlock} addTransaction={addTransaction}/>} />
+      </Routes>
+    </Router>
   );
-};
+}
